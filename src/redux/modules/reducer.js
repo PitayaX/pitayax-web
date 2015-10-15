eab212c991669d5270f200f01cbc46158a869d7f
Tim Huang <tim.shanghai@outlook.com>
2015-10-15 18:10:15 +0800

    add redux

--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -5,11 +5,12 @@ import counter from './counter'
 import { reducer as form } from 'redux-form'
 import info from './info'
 import widgets from './widgets'
-
+import singleArticle from './singleArticle'
 export default combineReducers({
   auth,
   counter,
   form,
   info,
-  widgets
+  widgets,
+  singleArticle
 })
