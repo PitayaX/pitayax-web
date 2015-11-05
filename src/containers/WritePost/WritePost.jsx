d7ce57c3c971dacdfc010b9a387e905e8d89da2c
leozdgao <gzdleo0816@gmail.com>
2015-11-05 14:39:57 +0800

    remove postinstall script in npm

--- src/containers/WritePost/WritePost.jsx
+++ src/containers/WritePost/WritePost.jsx
@@ -25,7 +25,7 @@ class WritePost extends Component {
           <div className={styles.strip}></div>
         </div>
         <Editor ref='content' />
-        <button className={styles.editorButton} onClick={::this._clickToPostArticle}>递交</button>
+        <button className='btn btn-default' onClick={::this._clickToPostArticle}>递交</button>
       </div>
     )
   }
@@ -38,7 +38,6 @@ class WritePost extends Component {
     for (const field in article) {
       if (article.hasOwnProperty(field)) {
         if ( this._isEmptyString(article[field]) ) {
-          console.log(field)
           this._remindRequiredField(field)
           return
         }
@@ -50,20 +49,17 @@ class WritePost extends Component {
   _sendArticle (article) {
     const url = 'http://10.10.73.207:8088/api/post/create'
     const ajaxSender = request.post(url, article)
-    console.log(article)
     ajaxSender.then((res) => {
-      console.log(res)
     }).catch((err) => {
-      console.log(err)
+
     })
   }
 
   _isEmptyString (field) {
-    return field.replace(/(^\s*)|(\s*$)/g, '').length == 0
+    return field.replace(/(^\s*)|(\s*$)/g, '').length === 0
   }
 
   _remindRequiredField (fieldName) {
-    console.log(this.refs[fieldName])
   }
 }
 
