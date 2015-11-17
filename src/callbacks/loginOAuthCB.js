624c47bdaee60eafd0e649423c88960f6fa85a8d
Tim Huang <tim.shanghai@outlook.com>
2015-11-17 10:26:35 +0800

    Revert "re"
    This reverts commit 6c582d63d427a65c0b8dde876e8238fec7749fbe.

--- src/callbacks/loginOAuthCB.js
+++ src/callbacks/loginOAuthCB.js
@@ -1,166 +1,70 @@
-import http from 'http'
 import qs from 'querystring'
-import { aq as aq } from 'pitayax-service-core'
+import { aq } from 'pitayax-service-core'
 import cookie from 'cookie'
+import config from '../config'
 
 let COOKIES = []
-const COOKIE_KEY = 'Set-Cookie'
-
+const oAuthTokenUri = config.oAuthServer + '/token'
 
 export default function loginOAuthSuccess (req, res) {
 
-
-  COOKIES = req.headers[COOKIE_KEY] || []
-
-  // ... send http post to get oAuth token
-
-  getOAuthToken(req.query.code, (tokenInfo) => {
-    const expires =  new Date()
-    const expiresMSeconds = (Number(tokenInfo.expires_in) || 0) * 1000
-    expires.setTime(expires.getTime() + expiresMSeconds)
-
-    // ... notice client login success to close the modal
-
-    setCookie('access_token', tokenInfo.access_token, { httpOnly: true, path: '/', expires })
-
-    writeCookie(res)
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end("<script>window.top.refreshToken(10000)</script>")
-    // res.end("<script>window.top.refreshToken(" +expiresMSeconds+ ")</script>")
-  })
-}
-
-function getOAuthToken (code, SUCC_callback) {
-  const post_data = JSON.stringify({
-    code,
+  COOKIES = req.headers['Set-Cookie'] || []
+  postToOAuth(oAuthTokenUri, {
+    code: req.query.code,
     grant_type: "authorization_code",
     client_id: "pitayax-web",
-    redirect_uri: "http://10.10.73.3:3000/cb/login"
-
-  })
-  const post_options = {
-    protocol: 'http:',
-    host: '120.24.58.42',
-    port: '2001',
-    path: '/token',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
+    redirect_uri: config.oAuthRedirectUrl
+  }, tokenInfo => {
+    // ... notice client login success to close the modal
+    if (tokenInfo.error) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.end("login failed:"+ tokenInfo.error)
+    } else {
+      writeHeaderInfo(res, tokenInfo)
+      // res.end("<script>window.top.refreshToken(5000)</script>")
+      res.end("<script>window.top.refreshToken(" +expiresMSeconds+ ")</script>")
     }
-  }
-  const post_req =  http.request(post_options, function (post_res) {
-    post_res.setEncoding('utf8')
-    let body = ''
-    post_res.on('data', function (chunk) {
-      body += chunk
-    })
-    post_res.on('end', function () {
-      body = JSON.parse(body)
-      SUCC_callback(body)
-    })
-
-    post_res.on('error', function (e) {
-      console.log("problem to get token: " + e.message)
-    })
   })
-  post_req.write(post_data)
-  post_req.end()
-}
-
-function writeCookie (res) {
-  res.setHeader(COOKIE_KEY, COOKIES)
-}
-
-function setCookie ( name, value, options ) {
-  const cookies = COOKIES
-  const cookie = [ name, "=", value, ";" ]
-
-  options = options || {}
-
-  if ( options.expires ) {
-    cookie.push( " expires=", options.expires.toUTCString(), ";" )
-  }
-
-  if ( options.path ) {
-    cookie.push( " path=", options.path, ";" )
-  }
-
-  if ( options.domain ) {
-    cookie.push( " domain=", options.domain, ";" )
-  }
-
-  if ( options.secure ) {
-    cookie.push( " secure", ";" )
-  }
-
-  if ( options.httpOnly ) {
-
-    cookie.push( " httponly" )
-  }
-  cookies.push(cookie.join(""))
 }
 
 export function refreshToken (req, res) {
-
-
-
   const cookies = cookie.parse(req.headers.cookie)
 
-  console.log("refresh before: " + cookies.access_token)
-
-  refreshOAuthToken({
-    refresh_token: cookies.access_token,
-    grant_type: 'refresh_token'
+  postToOAuth(oAuthTokenUri, {
+    "refresh_token": cookies.access_token,
+    "grant_type": 'refresh_token'
   }, tokenInfo => {
-
-    const expires =  new Date()
-    const expiresMSeconds = (Number(tokenInfo.expires_in) || 0) * 1000
-    expires.setTime(expires.getTime() + expiresMSeconds)
-
-    // ... notice client login success to close the modal
-
-    setCookie('access_token', tokenInfo.access_token, { httpOnly: true, path: '/', expires })
-
-    console.log("refresh after: " + tokenInfo.access_token)
-
-    writeCookie(res)
-
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-
-    // res.end("<script>window.top.refreshToken(" +expiresMSeconds+ ")</script>")
-    res.end("window.top.refreshToken(10000)")
+    if (tokenInfo.error) {
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.end("window.top.refreshTokenFailed")
+    } else {
+      writeHeaderInfo(res, tokenInfo)
+      res.end("<script>window.top.refreshToken(" +expiresMSeconds+ ")</script>")
+      // res.end("window.top.refreshToken(5000)")
+    }
   })
 }
 
 
-function refreshOAuthToken (body, SUCC_callback) {
+function writeHeaderInfo (res, tokenInfo) {
+  const expires =  new Date()
+  const expiresMSeconds = (Number(tokenInfo.expires_in) || 0) * 1000
+  expires.setTime(expires.getTime() + expiresMSeconds)
+  COOKIES.push(cookie.serialize('access_token', tokenInfo.access_token, { httpOnly: true, path: '/', expires }))
+  res.setHeader('Set-Cookie', COOKIES)
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+}
 
-  const post_data = JSON.stringify(body)
-  const post_options = {
-    protocol: 'http:',
-    host: '120.24.58.42',
-    port: '2001',
-    path: '/token',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
+
+function postToOAuth (uri, data, cb) {
+  aq.rest(uri, "POST", {}, data)
+  .then(data => {
+    if (data.error) {
+      console.log("get token failed: " + data.error)
+      cb(data)
+    }else {
+      cb(data.data)
     }
-  }
-  const post_req =  http.request(post_options, function (post_res) {
-    post_res.setEncoding('utf8')
-    let body = ''
-    post_res.on('data', function (chunk) {
-      body += chunk
-    })
-    post_res.on('end', function () {
-      body = JSON.parse(body)
-      SUCC_callback(body)
-    })
-
-    post_res.on('error', function (e) {
-      console.log("problem to get token: " + e.message)
-    })
   })
-  post_req.write(post_data)
-  post_req.end()
+  .catch(e => console.log(e))
 }
